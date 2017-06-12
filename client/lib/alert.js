f7e47a776d984a340f1ccf73d24433c7e744934d
Pradel Léo <pradel.leo@gmail.com>
2017-06-12 11:02:40 +0200

    Add missing alert file

---
+++ client/lib/alert.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import Button from 'material-ui/Button';
+import Dialog, {
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from 'material-ui/Dialog';
+
+class Alert extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+      text: '',
+      title: '',
+    };
+    global.octonConfirm = this.showConfirm;
+  }
+
+  close = () => this.setState({ isOpen: false });
+
+  confirm = () => {
+    this.close();
+    this.resolve(true);
+  };
+
+  cancel = () => {
+    this.close();
+    this.resolve(false);
+  };
+
+  showConfirm = (title, text) =>
+    new Promise((resolve, reject) => {
+      this.setState({ isOpen: true, isConfirm: true, text, title });
+      this.resolve = resolve;
+      this.reject = reject;
+    });
+
+  render() {
+    const { isOpen, title, text } = this.state;
+    return (
+      <Dialog open={isOpen} onRequestClose={this.handleRequestClose}>
+        <DialogTitle>{title}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>{text}</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.confirm} accent>Confirm</Button>
+          <Button onClick={this.cancel}>Cancel</Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
+}
+
+export default Alert;
